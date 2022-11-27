2e1f288f56513e5c5667457f903f82e95997d5a2
sernics <alu0101389936@ull.edu.es>
2022-11-27 19:29:56 +0100

    Pequeños cambios que funcionan

--- filesysteminfo.sh
+++ filesysteminfo.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+are_usuarios=0
+usuarios=""
+
 function system_info() {
   echo "||-----------------------------------------------------------------------------------------------------------------------||"
   echo "||Filesistem                     |Dispositive name     |Storage              |Mount on                  |Total Used      ||"
@@ -54,12 +57,12 @@ function inverse() {
   echo "||Filesistem                     |Dispositive name     |Storage              |Mount on                  |Total Used      ||"
   echo "||-----------------------------------------------------------------------------------------------------------------------||"
   # Tipos es el tipo de sistema de archivos
-  tipos=$(cat /proc/mounts | cut -d  ' ' -f3 | sort -u | sort -r)
+  tipos=$(cat /proc/mounts | cut -d  ' ' -f3 | sort -u -r)
   tabla=""
   for tipo in $tipos; do
     line=$tipo
     line+=" "
-    line+=$(df -a -t $tipo | tr -s ' ' | tail -n +2 | sort -k3 -n | tail -n -1 | cut -d ' ' -f 1,3,6)
+    line+=$(df -a -t $tipo | tr -s ' ' | sort -k3 -n | tail -n -1 | cut -d ' ' -f 1,3,6)
     total_used=$(df -a -t $tipo | awk 'BEGIN {total=0} {total = total + $3} END {print total}')
     line=$(printf "||%-30s |%-20s |%-20s |%-25s |%-15s ||\n" $line $total_used)
     
@@ -82,6 +85,23 @@ if [ $# -gt 0 ]; then
     case $1 in
       "-h" | "--help")
         helper
+        shift
+        ;;
+      "-u")
+        shift
+        while [ "$1" != "" ]; do
+          # Comprobar que el valor de $1 es un usuario
+          if id "$1" >/dev/null 2>&1; then
+            are_usuarios=1
+            usuarios=$1
+            usuarios+=" "
+          else
+            echo "The user $1 is not a valid user"
+          fi
+          shift
+        done
+        echo $usuarios
+        echo $are_usuarios
         shift
         ;;
       "-inv" )
